7d531d8c7914b6cb9799b38fdc05294ea4381691
César Magalhães <cesar.magalhaes-ext@ab-inbev.com>
2021-12-22 10:17:31 -0300

    Refactory EmailVO unit tests

--- src/test/java/br/cams7/tests/ms/core/port/in/EmailVOTests.java
+++ src/test/java/br/cams7/tests/ms/core/port/in/EmailVOTests.java
@@ -20,6 +20,19 @@ public class EmailVOTests {
   private static final String WRONG_IDENTIFICATION_NUMBER = "56317992001";
   private static final String WRONG_EMAIL = "test";
 
+  private static final String ERROR_MESSAGE_BLANK_IDENTIFICATION_NUMBER =
+      "identificationNumber: must not be blank";
+  private static final String ERROR_MESSAGE_WRONG_IDENTIFICATION_NUMBER =
+      "identificationNumber: invalid Brazilian individual taxpayer registry number (CPF)";
+  private static final String ERROR_MESSAGE_BLANK_EMAIL_FROM = "emailFrom: must not be blank";
+  private static final String ERROR_MESSAGE_WRONG_EMAIL_FROM =
+      "emailFrom: must be a well-formed email address";
+  private static final String ERROR_MESSAGE_BLANK_EMAIL_TO = "emailTo: must not be blank";
+  private static final String ERROR_MESSAGE_WRONG_EMAIL_TO =
+      "emailTo: must be a well-formed email address";
+  private static final String ERROR_MESSAGE_BLANK_SUBJECT = "subject: must not be blank";
+  private static final String ERROR_MESSAGE_BLANK_TEXT = "text: must not be blank";
+
   @Test
   @DisplayName("create email instance when successfull")
   void createEmailInstance_WhenSuccessful() {
@@ -34,179 +47,53 @@ public class EmailVOTests {
   }
 
   @Test
-  @DisplayName("create email instance throws error when pass null \"identification number\"")
-  void createEmailInstance_ThrowsError_WhenPassNullIdentificationNumber() {
+  @DisplayName("create email instance throws error when pass null params")
+  void createEmailInstance_ThrowsError_WhenPassNullParams() {
     ConstraintViolationException thrown =
         assertThrows(
             ConstraintViolationException.class,
             () -> {
-              new EmailVO(null, EMAIL_FROM, EMAIL_TO, MESSAGE_SUBJECT, MESSAGE_TEXT);
+              new EmailVO(null, null, null, null, null);
             });
 
-    assertThat(thrown.getMessage()).isEqualTo("identificationNumber: must not be blank");
+    assertThat(thrown.getMessage()).contains(ERROR_MESSAGE_BLANK_IDENTIFICATION_NUMBER);
+    assertThat(thrown.getMessage()).contains(ERROR_MESSAGE_BLANK_EMAIL_FROM);
+    assertThat(thrown.getMessage()).contains(ERROR_MESSAGE_BLANK_EMAIL_TO);
+    assertThat(thrown.getMessage()).contains(ERROR_MESSAGE_BLANK_SUBJECT);
+    assertThat(thrown.getMessage()).contains(ERROR_MESSAGE_BLANK_TEXT);
   }
 
   @Test
-  @DisplayName("create email instance throws error when pass empty \"identification number\"")
-  void createEmailInstance_ThrowsError_WhenPassEmptyIdentificationNumber() {
+  @DisplayName("create email instance throws error when pass empty params")
+  void createEmailInstance_ThrowsError_WhenPassEmptyParams() {
     ConstraintViolationException thrown =
         assertThrows(
             ConstraintViolationException.class,
             () -> {
-              new EmailVO(EMPTY, EMAIL_FROM, EMAIL_TO, MESSAGE_SUBJECT, MESSAGE_TEXT);
+              new EmailVO(EMPTY, EMPTY, EMPTY, EMPTY, EMPTY);
             });
 
-    assertThat(thrown.getMessage()).contains("identificationNumber: must not be blank");
-    assertThat(thrown.getMessage())
-        .contains(
-            "identificationNumber: invalid Brazilian individual taxpayer registry number (CPF)");
+    assertThat(thrown.getMessage()).contains(ERROR_MESSAGE_BLANK_IDENTIFICATION_NUMBER);
+    assertThat(thrown.getMessage()).contains(ERROR_MESSAGE_BLANK_EMAIL_FROM);
+    assertThat(thrown.getMessage()).contains(ERROR_MESSAGE_BLANK_EMAIL_TO);
+    assertThat(thrown.getMessage()).contains(ERROR_MESSAGE_BLANK_SUBJECT);
+    assertThat(thrown.getMessage()).contains(ERROR_MESSAGE_BLANK_TEXT);
   }
 
   @Test
-  @DisplayName("create email instance throws error when pass wrong \"identification number\"")
-  void createEmailInstance_ThrowsError_WhenPassWrongIdentificationNumber() {
+  @DisplayName("create email instance throws error when pass wrong params")
+  void createEmailInstance_ThrowsError_WhenPassWrongParams() {
     ConstraintViolationException thrown =
         assertThrows(
             ConstraintViolationException.class,
             () -> {
-              new EmailVO(
-                  WRONG_IDENTIFICATION_NUMBER, EMAIL_FROM, EMAIL_TO, MESSAGE_SUBJECT, MESSAGE_TEXT);
+              new EmailVO(WRONG_IDENTIFICATION_NUMBER, WRONG_EMAIL, WRONG_EMAIL, EMPTY, EMPTY);
             });
 
-    assertThat(thrown.getMessage())
-        .isEqualTo(
-            "identificationNumber: invalid Brazilian individual taxpayer registry number (CPF)");
-  }
-
-  @Test
-  @DisplayName("create email instance throws error when pass null \"email from\"")
-  void createEmailInstance_ThrowsError_WhenPassNullEmailFrom() {
-    ConstraintViolationException thrown =
-        assertThrows(
-            ConstraintViolationException.class,
-            () -> {
-              new EmailVO(OWNER_REF, null, EMAIL_TO, MESSAGE_SUBJECT, MESSAGE_TEXT);
-            });
-
-    assertThat(thrown.getMessage()).isEqualTo("emailFrom: must not be blank");
-  }
-
-  @Test
-  @DisplayName("create email instance throws error when pass empty \"email from\"")
-  void createEmailInstance_ThrowsError_WhenPassEmptyEmailFrom() {
-    ConstraintViolationException thrown =
-        assertThrows(
-            ConstraintViolationException.class,
-            () -> {
-              new EmailVO(OWNER_REF, EMPTY, EMAIL_TO, MESSAGE_SUBJECT, MESSAGE_TEXT);
-            });
-
-    assertThat(thrown.getMessage()).contains("emailFrom: must not be blank");
-    assertThat(thrown.getMessage()).contains("emailFrom: must be a well-formed email address");
-  }
-
-  @Test
-  @DisplayName("create email instance throws error when pass wrong \"email from\"")
-  void createEmailInstance_ThrowsError_WhenPassWrongEmailFrom() {
-    ConstraintViolationException thrown =
-        assertThrows(
-            ConstraintViolationException.class,
-            () -> {
-              new EmailVO(OWNER_REF, WRONG_EMAIL, EMAIL_TO, MESSAGE_SUBJECT, MESSAGE_TEXT);
-            });
-
-    assertThat(thrown.getMessage()).isEqualTo("emailFrom: must be a well-formed email address");
-  }
-
-  @Test
-  @DisplayName("create email instance throws error when pass null \"email to\"")
-  void createEmailInstance_ThrowsError_WhenPassNullEmailTo() {
-    ConstraintViolationException thrown =
-        assertThrows(
-            ConstraintViolationException.class,
-            () -> {
-              new EmailVO(OWNER_REF, EMAIL_FROM, null, MESSAGE_SUBJECT, MESSAGE_TEXT);
-            });
-
-    assertThat(thrown.getMessage()).isEqualTo("emailTo: must not be blank");
-  }
-
-  @Test
-  @DisplayName("create email instance throws error when pass empty \"email to\"")
-  void createEmailInstance_ThrowsError_WhenPassEmptyEmailTo() {
-    ConstraintViolationException thrown =
-        assertThrows(
-            ConstraintViolationException.class,
-            () -> {
-              new EmailVO(OWNER_REF, EMAIL_FROM, EMPTY, MESSAGE_SUBJECT, MESSAGE_TEXT);
-            });
-
-    assertThat(thrown.getMessage()).contains("emailTo: must not be blank");
-    assertThat(thrown.getMessage()).contains("emailTo: must be a well-formed email address");
-  }
-
-  @Test
-  @DisplayName("create email instance throws error when pass wrong \"email to\"")
-  void createEmailInstance_ThrowsError_WhenPassWrongEmailTo() {
-    ConstraintViolationException thrown =
-        assertThrows(
-            ConstraintViolationException.class,
-            () -> {
-              new EmailVO(OWNER_REF, EMAIL_FROM, WRONG_EMAIL, MESSAGE_SUBJECT, MESSAGE_TEXT);
-            });
-
-    assertThat(thrown.getMessage()).isEqualTo("emailTo: must be a well-formed email address");
-  }
-
-  @Test
-  @DisplayName("create email instance throws error when pass null subject")
-  void createEmailInstance_ThrowsError_WhenPassNullSubject() {
-    ConstraintViolationException thrown =
-        assertThrows(
-            ConstraintViolationException.class,
-            () -> {
-              new EmailVO(OWNER_REF, EMAIL_FROM, EMAIL_TO, null, MESSAGE_TEXT);
-            });
-
-    assertThat(thrown.getMessage()).isEqualTo("subject: must not be blank");
-  }
-
-  @Test
-  @DisplayName("create email instance throws error when pass empty subject")
-  void createEmailInstance_ThrowsError_WhenPassEmptySubject() {
-    ConstraintViolationException thrown =
-        assertThrows(
-            ConstraintViolationException.class,
-            () -> {
-              new EmailVO(OWNER_REF, EMAIL_FROM, EMAIL_TO, EMPTY, MESSAGE_TEXT);
-            });
-
-    assertThat(thrown.getMessage()).isEqualTo("subject: must not be blank");
-  }
-
-  @Test
-  @DisplayName("create email instance throws error when pass null text")
-  void createEmailInstance_ThrowsError_WhenPassNullText() {
-    ConstraintViolationException thrown =
-        assertThrows(
-            ConstraintViolationException.class,
-            () -> {
-              new EmailVO(OWNER_REF, EMAIL_FROM, EMAIL_TO, MESSAGE_SUBJECT, null);
-            });
-
-    assertThat(thrown.getMessage()).isEqualTo("text: must not be blank");
-  }
-
-  @Test
-  @DisplayName("create email instance throws error when pass empty text")
-  void createEmailInstance_ThrowsError_WhenPassEmptyText() {
-    ConstraintViolationException thrown =
-        assertThrows(
-            ConstraintViolationException.class,
-            () -> {
-              new EmailVO(OWNER_REF, EMAIL_FROM, EMAIL_TO, MESSAGE_SUBJECT, EMPTY);
-            });
-
-    assertThat(thrown.getMessage()).isEqualTo("text: must not be blank");
+    assertThat(thrown.getMessage()).contains(ERROR_MESSAGE_WRONG_IDENTIFICATION_NUMBER);
+    assertThat(thrown.getMessage()).contains(ERROR_MESSAGE_WRONG_EMAIL_FROM);
+    assertThat(thrown.getMessage()).contains(ERROR_MESSAGE_WRONG_EMAIL_TO);
+    assertThat(thrown.getMessage()).contains(ERROR_MESSAGE_BLANK_SUBJECT);
+    assertThat(thrown.getMessage()).contains(ERROR_MESSAGE_BLANK_TEXT);
   }
 }
