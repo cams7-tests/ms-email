87cafcc6d2631eb453decebe0923c664ac88b8ba
César Magalhães <cesar.magalhaes-ext@ab-inbev.com>
2022-01-08 12:33:25 -0300

    Add swagger

--- src/main/java/br/cams7/tests/ms/infra/entrypoint/GetEmailController.java
+++ src/main/java/br/cams7/tests/ms/infra/entrypoint/GetEmailController.java
@@ -8,6 +8,10 @@ import br.cams7.tests.ms.core.port.pagination.OrderDTO;
 import br.cams7.tests.ms.core.port.pagination.PageDTO;
 import br.cams7.tests.ms.infra.entrypoint.mapper.ResponseConverter;
 import br.cams7.tests.ms.infra.entrypoint.response.EmailResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
@@ -23,9 +27,17 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
+@SecurityScheme(
+    name = GetEmailController.SECURITY_SCHEME_NAME,
+    type = SecuritySchemeType.HTTP,
+    scheme = GetEmailController.SECURITY_SCHEME_SCHEME)
 @RestController
 @RequiredArgsConstructor
 public class GetEmailController {
+
+  public static final String SECURITY_SCHEME_NAME = "Basic Authentication";
+  public static final String SECURITY_SCHEME_SCHEME = "basic";
+  public static final String OPERATION_TAGS = "ms-email";
 
   private static final int DEFAULT_PAGE_NUMBER = 0;
   private static final int DEFAULT_PAGE_SIZE = 5;
@@ -36,6 +48,10 @@ public class GetEmailController {
   private final GetEmailUseCase getEmailUseCase;
   private final ResponseConverter responseConverter;
 
+  @Operation(
+      summary = "List emails",
+      tags = {OPERATION_TAGS},
+      security = @SecurityRequirement(name = SECURITY_SCHEME_NAME))
   @GetMapping(path = "/emails")
   @ResponseStatus(HttpStatus.OK)
   Mono<PageDTO<EmailResponseDTO>> getEmails(
@@ -50,6 +66,10 @@ public class GetEmailController {
         .map(responseConverter::convert);
   }
 
+  @Operation(
+      summary = "Get the email by id",
+      tags = {OPERATION_TAGS},
+      security = @SecurityRequirement(name = SECURITY_SCHEME_NAME))
   @GetMapping(path = "/emails/{emailId}")
   @ResponseStatus(HttpStatus.OK)
   Mono<EmailResponseDTO> getEmail(@PathVariable(value = "emailId") final UUID emailId) {
--- src/main/java/br/cams7/tests/ms/infra/entrypoint/SendEmailController.java
+++ src/main/java/br/cams7/tests/ms/infra/entrypoint/SendEmailController.java
@@ -6,6 +6,10 @@ import br.cams7.tests.ms.core.port.in.SendEmailToQueueUseCase;
 import br.cams7.tests.ms.infra.entrypoint.mapper.ResponseConverter;
 import br.cams7.tests.ms.infra.entrypoint.request.SendEmailRequestDTO;
 import br.cams7.tests.ms.infra.entrypoint.response.EmailResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,14 +18,26 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
+@SecurityScheme(
+    name = SendEmailController.SECURITY_SCHEME_NAME,
+    type = SecuritySchemeType.HTTP,
+    scheme = SendEmailController.SECURITY_SCHEME_SCHEME)
 @RestController
 @RequiredArgsConstructor
 public class SendEmailController {
+
+  public static final String SECURITY_SCHEME_NAME = "Basic Authentication";
+  public static final String SECURITY_SCHEME_SCHEME = "basic";
+  public static final String OPERATION_TAGS = "ms-email";
 
   private final SendEmailDirectlyUseCase sendEmailDirectlyUseCase;
   private final SendEmailToQueueUseCase sendEmailToQueueUseCase;
   private final ResponseConverter responseConverter;
 
+  @Operation(
+      summary = "Send the email directly",
+      tags = {OPERATION_TAGS},
+      security = @SecurityRequirement(name = SECURITY_SCHEME_NAME))
   @PostMapping(path = "/send-email-directly")
   @ResponseStatus(HttpStatus.OK)
   Mono<EmailResponseDTO> sendEmailDirectly(
@@ -31,6 +47,10 @@ public class SendEmailController {
         .map(responseConverter::convert);
   }
 
+  @Operation(
+      summary = "Send the email indirectly",
+      tags = {OPERATION_TAGS},
+      security = @SecurityRequirement(name = SECURITY_SCHEME_NAME))
   @PostMapping(path = "/send-email-to-queue")
   @ResponseStatus(HttpStatus.OK)
   Mono<Void> sendEmailToQueue(@RequestBody final SendEmailRequestDTO sendEmailRequest) {
