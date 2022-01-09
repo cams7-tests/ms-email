1f91e53262d31d44d4a90f377c871e0f949c4d17
César Magalhães <cesar.magalhaes-ext@ab-inbev.com>
2022-01-09 17:02:36 -0300

    Fix Code Smell

--- src/main/java/br/cams7/tests/ms/infra/logging/LoggingAspect.java
+++ src/main/java/br/cams7/tests/ms/infra/logging/LoggingAspect.java
@@ -127,10 +127,10 @@ public class LoggingAspect {
     var response = point.proceed();
     if (response != null) {
       if (response instanceof Mono || response instanceof Flux) {
-        if (response instanceof Mono) {
+        if (response instanceof Mono)
           response =
               ((Mono<?>) response).doOnNext(data -> exitLog(logger, methodName, start, data));
-        } else
+        else
           response =
               ((Flux<?>) response).doOnNext(data -> exitLog(logger, methodName, start, data));
       } else exitLog(logger, methodName, start, response);
