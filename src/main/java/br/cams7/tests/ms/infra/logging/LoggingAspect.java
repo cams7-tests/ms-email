1b3e97177af521f630d87b254f7651322b261004
César Magalhães <cesar.magalhaes-ext@ab-inbev.com>
2022-01-09 16:51:33 -0300

    Update LoggingAspect class

--- src/main/java/br/cams7/tests/ms/infra/logging/LoggingAspect.java
+++ src/main/java/br/cams7/tests/ms/infra/logging/LoggingAspect.java
@@ -53,30 +53,21 @@ public class LoggingAspect {
     Method method = methodSignature.getMethod();
     var annotation = method.getAnnotation(LogEntryExit.class);
 
-    return log(
-        showLog,
-        point,
-        annotation.value(),
-        annotation.unit(),
-        annotation.showArgs(),
-        annotation.showResult(),
-        annotation.showExecutionTime());
+    showArgs = annotation.showArgs();
+    showResult = annotation.showResult();
+    showExecutionTime = annotation.showExecutionTime();
+    level = annotation.value();
+    unit = annotation.unit();
+
+    return log(point);
   }
 
   @Around("execution(* " + USECASE_PATH + "..*(..)))")
   public Object logMethod(ProceedingJoinPoint point) throws Throwable {
-    return log(showLog, point, level, unit, showArgs, showResult, showExecutionTime);
+    return log(point);
   }
 
-  private static Object log(
-      boolean showLog,
-      ProceedingJoinPoint point,
-      LogLevel level,
-      ChronoUnit unit,
-      boolean showArgs,
-      boolean showResult,
-      boolean showExecutionTime)
-      throws Throwable {
+  private Object log(ProceedingJoinPoint point) throws Throwable {
     if (!showLog) return point.proceed();
     var codeSignature = (CodeSignature) point.getSignature();
     var methodSignature = (MethodSignature) point.getSignature();
@@ -90,88 +81,11 @@ public class LoggingAspect {
 
     if (unit == null) unit = MILLIS;
 
-    log(logger, level, entry(methodName, showArgs, methodParams, methodArgs));
-    return getResponse(point, level, unit, showResult, showExecutionTime, logger, methodName);
+    log(logger, entry(methodName, methodParams, methodArgs));
+    return getResponse(point, logger, methodName);
   }
 
-  private static String entry(String methodName, boolean showArgs, String[] params, Object[] args) {
-    var message = new StringJoiner(" ").add("Started").add(methodName).add("method");
-    if (showArgs
-        && Objects.nonNull(params)
-        && Objects.nonNull(args)
-        && params.length == args.length) {
-      var values = new HashMap<>(params.length);
-      for (int i = 0; i < params.length; i++) {
-        values.put(params[i], args[i]);
-      }
-      message.add("with args:").add(values.toString());
-    }
-    return message.toString();
-  }
-
-  private static Object getResponse(
-      ProceedingJoinPoint point,
-      LogLevel level,
-      ChronoUnit unit,
-      boolean showResult,
-      boolean showExecutionTime,
-      Logger logger,
-      String methodName)
-      throws Throwable {
-    var start = Instant.now();
-    var response = point.proceed();
-    if (response != null)
-      if (response instanceof Mono || response instanceof Flux) {
-        if (response instanceof Mono) {
-          response =
-              ((Mono<?>) response)
-                  .doOnNext(
-                      data ->
-                          log(
-                              level,
-                              unit,
-                              showResult,
-                              showExecutionTime,
-                              logger,
-                              methodName,
-                              start,
-                              data));
-        } else {
-          response =
-              ((Flux<?>) response)
-                  .doOnNext(
-                      data ->
-                          log(
-                              level,
-                              unit,
-                              showResult,
-                              showExecutionTime,
-                              logger,
-                              methodName,
-                              start,
-                              data));
-        }
-      } else {
-        log(level, unit, showResult, showExecutionTime, logger, methodName, start, response);
-      }
-    return response;
-  }
-
-  private static void log(
-      LogLevel level,
-      ChronoUnit unit,
-      boolean showResult,
-      boolean showExecutionTime,
-      Logger logger,
-      String methodName,
-      Instant start,
-      Object response) {
-    var end = Instant.now();
-    var duration = String.format("%s %s", unit.between(start, end), unit.name().toLowerCase());
-    log(logger, level, exit(methodName, duration, response, showResult, showExecutionTime));
-  }
-
-  private static void log(Logger logger, LogLevel level, String message) {
+  private void log(Logger logger, String message) {
     switch (level) {
       case DEBUG:
         logger.debug(message);
@@ -192,18 +106,52 @@ public class LoggingAspect {
     }
   }
 
-  private static String exit(
-      String methodName,
-      String duration,
-      Object result,
-      boolean showResult,
-      boolean showExecutionTime) {
+  private String entry(String methodName, String[] params, Object[] args) {
+    var message = new StringJoiner(" ").add("Started").add(methodName).add("method");
+    if (showArgs
+        && Objects.nonNull(params)
+        && Objects.nonNull(args)
+        && params.length == args.length) {
+      var values = new HashMap<>(params.length);
+      for (int i = 0; i < params.length; i++) {
+        values.put(params[i], args[i]);
+      }
+      message.add("with args:").add(values.toString());
+    }
+    return message.toString();
+  }
+
+  private Object getResponse(ProceedingJoinPoint point, Logger logger, String methodName)
+      throws Throwable {
+    var start = Instant.now();
+    var response = point.proceed();
+    if (response != null)
+      if (response instanceof Mono || response instanceof Flux) {
+        if (response instanceof Mono)
+          response =
+              ((Mono<?>) response).doOnNext(data -> exitLog(logger, methodName, start, data));
+        else
+          response =
+              ((Flux<?>) response).doOnNext(data -> exitLog(logger, methodName, start, data));
+      } else {
+        exitLog(logger, methodName, start, response);
+      }
+    return response;
+  }
+
+  private void exitLog(Logger logger, String methodName, Instant start, Object response) {
+    log(logger, exit(methodName, start, response));
+  }
+
+  private String exit(String methodName, Instant start, Object response) {
+    var end = Instant.now();
+    var duration = String.format("%s %s", unit.between(start, end), unit.name().toLowerCase());
     var message = new StringJoiner(" ").add("Finished").add(methodName).add("method");
     if (showExecutionTime) {
       message.add("in").add(duration);
     }
     if (showResult) {
-      message.add("with return:").add(result.toString());
+      message.add("with return:").add(response.toString());
     }
     return message.toString();
   }
